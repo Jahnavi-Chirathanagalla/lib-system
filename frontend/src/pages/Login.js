13d208bc755659ffd9bb43a9b9714486ac8e7aed
Jahnavi-Chirathanagalla <jahnavichiratanagalla@gmail.com>
2026-03-05 06:47:29 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://lib-system-backend.onrender.com",
         credentials
       );
       localStorage.setItem("token", res.data.token);
